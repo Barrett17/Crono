9bf4d33783c4173ecaf79fc7ec461d6c8b1dc0d4
Barrett <barrett666@gmail.com>
2013-04-08 13:13:42 +0200

    Add missing file CronoDefaults.h

---
+++ CronoDefaults.h
@@ -0,0 +1,10 @@
+#ifndef CRONO_SETTINGS_H
+#define CRONO_SETTINGS_H
+
+const int32		DEFAULT_SPEED	=	60;
+const int32		DEFAULT_VOLUME	=	80;
+
+#define CRONO_TIC_LOCATION "audio/tic.wav"
+#define CRONO_TOC_LOCATION "audio/toc.wav"
+
+#endif
